b0e830191dd9fad99eb13fe05b9def6d3e09c0b8
Caio Mizumoto <caio.mizumoto@gmail.com>
2022-11-09 18:47:12 -0300

    created authentication page component, with sign in form, renamed folders for better meaning

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -2,16 +2,18 @@ import { useEffect } from "react";
 import { getRedirectResult } from "firebase/auth";
 
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
-import {
-  auth,
-  signInWithGooglePopup,
-  // signInWithGoogleRedirect,
-  createUserDocumentFromAuth,
-} from "../../utils/firebase/firebase.utils";
+import "./authentication.styles.scss";
+
+// import {
+//   auth,
+//   signInWithGoogleRedirect,
+//   createUserDocumentFromAuth,
+// } from "../../utils/firebase/firebase.utils";
 
 /* Creating user in the firestore */
-const SignIn = () => {
+const Authentication = () => {
   // /* This useEffect unsures that when the user comes back from the google redirect, this code executes and we receive the necessary information to create the user in the data base */
   // useEffect(async () => {
   //   const response = await getRedirectResult(auth);
@@ -20,22 +22,19 @@ const SignIn = () => {
   //   }
   // }, []);
 
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user);
-  };
   /*  const logGoogleRedirectUser = async () => {
     const { user } = await signInWithGoogleRedirect();
     console.log({ user });
   }; */
   return (
-    <div>
-      <h1>Sign in page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+    <div className="authentication-container">
+      {/* <h1>Sign in page</h1> */}
+      {/* <button onClick={logGoogleUser}>Sign in with Google Popup</button> */}
       {/* <button onClick={logGoogleRedirectUser}>Sign in with Google Redirect</button> */}
+      <SignInForm />
       <SignUpForm />
     </div>
   );
 };
 
-export default SignIn;
+export default Authentication;
