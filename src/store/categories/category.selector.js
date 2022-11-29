11ab7e193c0e943821faf9949cea774617f8d71f
Caio Mizumoto <caio.mizumoto@gmail.com>
2022-11-29 11:47:30 -0300

    adding reselect library, refactoring category selector to use reselect

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,28 @@
-export const selectCategoriesMap = (state) =>
-  state.categories.categories.reduce((accumulator, category) => {
+import { createSelector } from "reselect";
+
+/* 
+  In a example, if a user logout at the Shop route, all selectors will be fired.
+  Now that we setup the reselect library, since nothing has changed in the category,
+  Once it reaches the selectCategories, this selector will already return the selectCategoryReducer
+  It won't even run the second argument of the function.
+*/
+const selectCategoryReducer = (state) => state.categories;
+
+/* 
+  Here we are using the reselect library to create a memoized function to avoid calculating the same state when other selectors are fired.
+  This function will receive two arguments:
+  1st argument is the selector that we are going to cache
+  2nd argument is a function that receives the return value of selectCategoriesReducer
+*/
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesFromReducer) => categoriesFromReducer.categories
+);
+
+export const selectCategoriesMap = createSelector([selectCategories], (categories) =>
+  categories.reduce((accumulator, category) => {
     const { title, items } = category;
     accumulator[title.toLowerCase()] = items;
     return accumulator;
-  }, {});
+  }, {})
+);
