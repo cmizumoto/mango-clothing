969017983bd8089e27b18ae571e6f5bafa81b02c
Caio Mizumoto <caio.mizumoto@gmail.com>
2023-01-02 15:20:43 -0300

    Added typescript to category action and reducer files, aswell the reducer utils

--- src/store/categories/category.reducer.ts
+++ src/store/categories/category.reducer.ts
@@ -1,6 +1,11 @@
-import { CATEGORIES_ACTION_TYPES, Category } from "./category.types";
+import { AnyAction } from "redux";
+import { Category } from "./category.types";
 
-import { CategoryAction } from "./category.action";
+import {
+  fetchCategoriesStart,
+  fetchCategoriesSuccess,
+  fetchCategoriesFailed,
+} from "./category.action";
 
 // Here we show how CATEGORY_INITIAL_STATE types are
 export type CategoriesState = {
@@ -19,26 +24,47 @@ export const CATEGORY_INITIAL_STATE: CategoriesState = {
 /* 
   By putting the "as CategoryAction" it becomes a Discriminating Union (https://basarat.gitbook.io/typescript/type-system/discriminated-unions)
   And will only accept the only 3 actions we set in category.action (FetchCategoriesStart, FetchCategoriesSuccess, FetchCategoriesFailed)
+  ==========
+  We refactored the code, so we don't have the problem where other action types, mess up with this reducer
 */
 export const categoriesReducer = (
   state = CATEGORY_INITIAL_STATE,
-  action = {} as CategoryAction
-) => {
+  action = {} as AnyAction
+): CategoriesState => {
   // Since our Action was not receiving a payload type, we can remove and specify the action inside our switch
   // With action.type and action.payload
   // const { type, payload } = action;
-  switch (action.type) {
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
-      return { ...state, isLoading: true };
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-      return {
-        ...state,
-        isLoading: false,
-        categories: action.payload,
-      };
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-      return { ...state, isLoading: false, error: action.payload };
-    default:
-      return state;
+
+  if (fetchCategoriesStart.match(action)) {
+    return { ...state, isLoading: true };
   }
+
+  if (fetchCategoriesSuccess.match(action)) {
+    return {
+      ...state,
+      isLoading: false,
+      categories: action.payload,
+    };
+  }
+
+  if (fetchCategoriesFailed.match(action)) {
+    return { ...state, isLoading: false, error: action.payload };
+  }
+
+  return state;
+
+  // switch (action.type) {
+  //   case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
+  //     return { ...state, isLoading: true };
+  //   case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
+  //     return {
+  //       ...state,
+  //       isLoading: false,
+  //       categories: action.payload,
+  //     };
+  //   case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
+  //     return { ...state, isLoading: false, error: action.payload };
+  //   default:
+  //     return state;
+  // }
 };
--- src/utils/reducer/reducer.utils.ts
+++ src/utils/reducer/reducer.utils.ts
@@ -8,6 +8,55 @@ export const createAction = (type, payload) => ({ type, payload });
  */
 import { AnyAction } from "redux";
 
+/* 
+  Intersection "&" - creates an hybrid with two types
+  type Human = {name: string};
+  type Alien = {fly: () => void};
+  type Hybrid = Human & Alien;
+  =============
+  ReturnType - gets the return type of the defined function
+  type MyFunction = () => string;
+  type MyReturn = ReturnType<MyFunction> // type MyReturn = string;
+*/
+/* 
+  Now here we use both Intersection and ReturnType to create our matchable type, to make the actions work
+  Get the type by accessing the 'type' inside the actions
+  the match are defined bellow this type Matchable
+*/
+type Matchable<AC extends () => AnyAction> = AC & {
+  type: ReturnType<AC>["type"];
+  match(action: AnyAction): action is ReturnType<AC>;
+};
+
+/* 
+  These three functions are made to overload and match with the action types, with and without arguments
+  With the first one is without any arguments made for the starting actions
+  Second is with arguments, and since we can receive any kind of data, we declare with "any[]"
+*/
+export function withMatcher<AC extends () => AnyAction & { type: string }>(
+  actionCreator: AC
+): Matchable<AC>;
+
+export function withMatcher<AC extends (...args: any[]) => AnyAction & { type: string }>(
+  actionCreator: AC
+): Matchable<AC>;
+
+/* 
+  This third function is defining what the withMatcher really does.
+  A function that receives an actionCreator that is a function
+  Gets the type by accessing the actionCreator type, because they always return an action that has a type
+  Now returning a mappable object, with the type and ReturnType
+*/
+export function withMatcher(actionCreator: Function) {
+  const type = actionCreator().type;
+  return Object.assign(actionCreator, {
+    type,
+    match(action: AnyAction) {
+      return action.type === type;
+    },
+  });
+}
+
 export type ActionWithPayload<T, P> = {
   type: T;
   payload: P;
